34c97e52f5a59978d67bdeb304258594d8839b04
Jochem Rebergen <jochemrebergen@gmail.com>
2014-09-23 09:17:23 +0200

    Update basic scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name appvailabilityApp
- * @description
- * # appvailabilityApp
- *
- * Main module of the application.
- */
 angular
   .module('appvailabilityApp', [
     'ngAnimate',
@@ -26,7 +18,8 @@ angular
       })
       .when('/members', {
         templateUrl: 'views/members.html',
-        controller: 'AboutCtrl'
+        controller: 'MembersCtrl',
+        controllerAs: 'membersCtrl'
       })
       .otherwise({
         redirectTo: '/'
