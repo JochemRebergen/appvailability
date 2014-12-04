058bdc9a8bea8334bfd3e5c045c4783ad58e7e64
Jochem Rebergen <jochemrebergen@gmail.com>
2014-12-04 14:57:05 +0100

    Added 'add' and 'delete' users

--- app/scripts/controllers/members.js
+++ app/scripts/controllers/members.js
@@ -1,14 +1,40 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name appvailabilityApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the appvailabilityApp
- */
 angular.module('appvailabilityApp')
-  .controller('MembersCtrl', function () {
+  .controller('MembersCtrl', function (dataService) {
+    var self = this;
+
+    self.init = function() {
+      self.users = dataService.getData();
+    };
+
+    self.addUser = function() {
+      // add empty user object
+      var user = {};
+
+      // fill new name node with username from view
+      user.name = self.newUser;
+      user.availability = [];
+
+      for( var i=0; i<5; i++) {
+        user.availability.push({ state: 'available' });        
+      }
+
+      // push new 
+      self.users.push(user);
+
+      // save
+      dataService.setData(self.users);
+    };
+
+    self.deleteUser = function (user) {
+      // delete specific user
+      self.users.splice(self.users.indexOf(user), 1);
+      
+      //save
+      dataService.setData(self.users);
+    };
+
 
   });
 
--- app/scripts/controllers/overview.js
+++ app/scripts/controllers/overview.js
@@ -4,24 +4,28 @@ angular.module('appvailabilityApp')
   .controller('OverviewCtrl', function (dataService) {
     var self = this;
 
-    self.users = [{
-      name: 'John Doe',
-      availability: [{
-        state: 'not-available'
-      },
-      {
-        state: 'not-available-am' 
-      }]
-    },
-    {
-      name: 'Jane Smith',
-      availability: [{
-        state: 'available'
-      },
-      {
-        state: 'not-available' 
-      }]
-    }];
+    self.init = function() {
+      self.users = dataService.getData();
+    };
+
+    // self.users = [{
+    //   name: 'John Doe',
+    //   availability: [{
+    //     state: 'not-available'
+    //   },
+    //   {
+    //     state: 'not-available-am' 
+    //   }]
+    // },
+    // {
+    //   name: 'Jane Smith',
+    //   availability: [{
+    //     state: 'available'
+    //   },
+    //   {
+    //     state: 'not-available' 
+    //   }]
+    // }];
 
     self.saveState = function() {
       dataService.setData(self.users);
